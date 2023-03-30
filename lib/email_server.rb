6d6cb01e4799b8569092183d6570d4c9ea83e0dc
Federico Iachetti <iachetti.federico@gmail.com>
2023-03-30 12:33:51 -0300

    From Scratch

--- lib/email_server.rb
+++ lib/email_server.rb
@@ -4,34 +4,11 @@ class EmailServer
   end
 
   def create_account_pattern(pattern)
-    @account_group << pattern
+    raise NotImplementedError
   end
-
-  def receive!(emails=[])
-    @received_emails.concat(account_group.received_emails_from(emails))
-    @unreceived_emails.concat(account_group.unreceived_emails_from(emails))
-  end
-
-  boolean_count(:received_emails)
-  boolean_count(:unreceived_emails)
-
-  def received_emails_for_account?(account)
-    !received_emails_count_for_account(account).zero?
-  end
-
-  def received_emails_count_for_account(searched_account)
-    received_emails.count_for_account(searched_account)
-  end
-
+  
   private
-
-  attr_reader :received_emails
-  attr_reader :unreceived_emails
-  attr_reader :account_group
-
+  
   def initialize
-    @received_emails   = EmailSet[]
-    @unreceived_emails = EmailSet[]
-    @account_group = AccountGroup.new
   end
 end
--- spec/suite/functional/pattern_email_accounts_spec.rb
+++ spec/suite/functional/pattern_email_accounts_spec.rb
@@ -11,117 +11,124 @@ RSpec.describe "pattern email accounts" do
 
     server.receive!
 
-    expect(server).to boolean_count(0, :received_emails)
-    expect(server).to boolean_count(0, :unreceived_emails)
+    expected_view = <<~PLAIN
+      Received Emails:
+        Empty
+      Unreceived Emails:
+        Empty
+    PLAIN
+    
+    # expect(server).to boolean_count(0, :received_emails)
+    # expect(server).to boolean_count(0, :unreceived_emails)
   end
 
-  it "can receive emails for an account" do
-    server.create_account_pattern("pepe")
+  # it "can receive emails for an account" do
+  #   server.create_account_pattern("pepe")
 
-    server.receive!([email_factory.to_pepe, email_factory.to_pepe])
+  #   server.receive!([email_factory.to_pepe, email_factory.to_pepe])
 
-    expect(server).to boolean_count(2, :received_emails)
-    expect(server).to boolean_count(0, :unreceived_emails)
-  end
+  #   expect(server).to boolean_count(2, :received_emails)
+  #   expect(server).to boolean_count(0, :unreceived_emails)
+  # end
 
-  it "is case insensitive" do
-    server.create_account_pattern("PepE")
+  # it "is case insensitive" do
+  #   server.create_account_pattern("PepE")
 
-    server.receive!([email_factory.to_pepe, email_factory.to_pepe])
+  #   server.receive!([email_factory.to_pepe, email_factory.to_pepe])
 
-    expect(server).to boolean_count(2, :received_emails)
-    expect(server).to boolean_count(0, :unreceived_emails)
-  end
+  #   expect(server).to boolean_count(2, :received_emails)
+  #   expect(server).to boolean_count(0, :unreceived_emails)
+  # end
 
-  it "it adds new emails to the list if it receives at separate times" do
-    server.create_account_pattern("pepe")
+  # it "it adds new emails to the list if it receives at separate times" do
+  #   server.create_account_pattern("pepe")
 
-    server.receive!([email_factory.to_pepe, email_factory.to_pepe])
-    server.receive!([email_factory.to_pepe])
+  #   server.receive!([email_factory.to_pepe, email_factory.to_pepe])
+  #   server.receive!([email_factory.to_pepe])
 
-    expect(server).to boolean_count(3, :received_emails)
-  end
+  #   expect(server).to boolean_count(3, :received_emails)
+  # end
 
-  it "can unreceive emails sent to an nonexistent account" do
-    server.create_account_pattern("pepe1")
+  # it "can unreceive emails sent to an nonexistent account" do
+  #   server.create_account_pattern("pepe1")
 
-    server.receive!([email_factory.to_pepe(suffix: 1)])
+  #   server.receive!([email_factory.to_pepe(suffix: 1)])
 
-    expect(server).to boolean_count(1, :received_emails)
-    expect(server).to boolean_count(0, :unreceived_emails)
+  #   expect(server).to boolean_count(1, :received_emails)
+  #   expect(server).to boolean_count(0, :unreceived_emails)
 
-    expect(server.received_emails_count_for_account("pepe")).to eq(0)
-    expect_false(server.received_emails_for_account?("pepe"))
+  #   expect(server.received_emails_count_for_account("pepe")).to eq(0)
+  #   expect_false(server.received_emails_for_account?("pepe"))
 
-    expect(server.received_emails_count_for_account("pepe1")).to eq(1)
-    expect_true(server.received_emails_for_account?("pepe1"))
-  end
+  #   expect(server.received_emails_count_for_account("pepe1")).to eq(1)
+  #   expect_true(server.received_emails_for_account?("pepe1"))
+  # end
 
-  it "has unreceived emails if an account haven't been set" do
-    server.receive!([email_factory.to_pepe, email_factory.to_pepe])
+  # it "has unreceived emails if an account haven't been set" do
+  #   server.receive!([email_factory.to_pepe, email_factory.to_pepe])
 
-    expect(server).to boolean_count(0, :received_emails)
-    expect(server).to boolean_count(2, :unreceived_emails)
-  end
+  #   expect(server).to boolean_count(0, :received_emails)
+  #   expect(server).to boolean_count(2, :unreceived_emails)
+  # end
 
-  it "can use a one-character suffix wildcard" do
-    server.create_account_pattern("pepe*")
+  # it "can use a one-character suffix wildcard" do
+  #   server.create_account_pattern("pepe*")
 
-    server.receive!([email_factory.to_pepe, email_factory.to_pepe(suffix: 1)])
+  #   server.receive!([email_factory.to_pepe, email_factory.to_pepe(suffix: 1)])
 
-    expect(server).to boolean_count(2, :received_emails)
-    expect(server).to boolean_count(0, :unreceived_emails)
+  #   expect(server).to boolean_count(2, :received_emails)
+  #   expect(server).to boolean_count(0, :unreceived_emails)
 
-    expect(server.received_emails_count_for_account("pepe")).to eq(1)
-    expect_true(server.received_emails_for_account?("pepe"))
+  #   expect(server.received_emails_count_for_account("pepe")).to eq(1)
+  #   expect_true(server.received_emails_for_account?("pepe"))
 
-    expect(server.received_emails_count_for_account("pepe1")).to eq(1)
-    expect_true(server.received_emails_for_account?("pepe1"))
+  #   expect(server.received_emails_count_for_account("pepe1")).to eq(1)
+  #   expect_true(server.received_emails_for_account?("pepe1"))
 
-    expect(server).to boolean_count(0, :unreceived_emails)
-  end
+  #   expect(server).to boolean_count(0, :unreceived_emails)
+  # end
 
-  it "can use a multiple-character suffix wildcard" do
-    server.create_account_pattern("pepe*")
+  # it "can use a multiple-character suffix wildcard" do
+  #   server.create_account_pattern("pepe*")
 
-    server.receive!([email_factory.to_pepe(suffix: "abc")])
+  #   server.receive!([email_factory.to_pepe(suffix: "abc")])
 
-    expect(server.received_emails_count_for_account("pepeabc")).to eq(1)
-    expect_true(server.received_emails_for_account?("pepeabc"))
-  end
+  #   expect(server.received_emails_count_for_account("pepeabc")).to eq(1)
+  #   expect_true(server.received_emails_for_account?("pepeabc"))
+  # end
 
-  it "can use a multiple-character prefix wildcard" do
-    server.create_account_pattern("*pepe")
+  # it "can use a multiple-character prefix wildcard" do
+  #   server.create_account_pattern("*pepe")
 
-    server.receive!([email_factory.to_pepe(prefix: "abc")])
+  #   server.receive!([email_factory.to_pepe(prefix: "abc")])
 
-    expect(server).to boolean_count(1, :received_emails)
+  #   expect(server).to boolean_count(1, :received_emails)
 
-    expect(server.received_emails_count_for_account("abcpepe")).to eq(1)
-    expect_true(server.received_emails_for_account?("abcpepe"))
-  end
+  #   expect(server.received_emails_count_for_account("abcpepe")).to eq(1)
+  #   expect_true(server.received_emails_for_account?("abcpepe"))
+  # end
 
-  it "can use a multiple-character prefix and suffix wildcard" do
-    server.create_account_pattern("*toto*")
+  # it "can use a multiple-character prefix and suffix wildcard" do
+  #   server.create_account_pattern("*toto*")
 
-    server.receive!([email_factory.to_toto(prefix: "abc", suffix: "def")])
+  #   server.receive!([email_factory.to_toto(prefix: "abc", suffix: "def")])
 
-    expect(server).to boolean_count(1, :received_emails)
+  #   expect(server).to boolean_count(1, :received_emails)
 
-    expect(server.received_emails_count_for_account("abctotodef")).to eq(1)
-    expect_true(server.received_emails_for_account?("abctotodef"))
-  end
+  #   expect(server.received_emails_count_for_account("abctotodef")).to eq(1)
+  #   expect_true(server.received_emails_for_account?("abctotodef"))
+  # end
 
-  it "can use a multiple-character prefix and suffix wildcard" do
-    server.create_account_pattern("*x*x*")
+  # it "can use a multiple-character prefix and suffix wildcard" do
+  #   server.create_account_pattern("*x*x*")
 
-    server.receive!([email_factory.email_to("aaxbbxcc")])
+  #   server.receive!([email_factory.email_to("aaxbbxcc")])
 
-    expect(server).to boolean_count(1, :received_emails)
+  #   expect(server).to boolean_count(1, :received_emails)
 
-    expect(server.received_emails_count_for_account("aaxbbxcc")).to eq(1)
-    expect_true(server.received_emails_for_account?("aaxbbxcc"))
-  end
+  #   expect(server.received_emails_count_for_account("aaxbbxcc")).to eq(1)
+  #   expect_true(server.received_emails_for_account?("aaxbbxcc"))
+  # end
 
   # TODO: fetching all emails
   # TODO: fetching emails for account
