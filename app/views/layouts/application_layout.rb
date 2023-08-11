90887077f670e074fde49150e309a024bd88d1e4
Federico Iachetti <iachetti.federico@gmail.com>
2023-08-11 10:25:36 -0300

    Reorganized layout

--- app/views/layouts/application_layout.rb
+++ app/views/layouts/application_layout.rb
@@ -12,12 +12,11 @@ class ApplicationLayout < ApplicationView
         meta(name: "viewport", content: "width=device-width,initial-scale=1")
         csp_meta_tag
         csrf_meta_tags
-        stylesheet_link_tag("application", data_turbo_track: "reload")
 
         javascript_importmap_tags
 
+        stylesheet_link_tag("application", data_turbo_track: "reload")
         stylesheet_link_tag("tailwind", "inter-font", "data-turbo-track": "reload")
-        stylesheet_link_tag "application"
       end
 
       body do
